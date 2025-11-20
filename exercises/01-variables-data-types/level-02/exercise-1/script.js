3106be88dd6f1aa6e3f6ab2cb6ea19a369507b17
Rene Alexandro FuentesMolina <renealexandrof@nycstudents.net>
2025-11-20 08:48:20 -0500

    prog

--- exercises/01-variables-data-types/level-02/exercise-1/script.js
+++ exercises/01-variables-data-types/level-02/exercise-1/script.js
@@ -1,3 +1,7 @@
 // TODO: Create variables for string, number, boolean, and undefined
+let text = "String";
+let num = any;
+let bool = Boolean;
+let undef = any;
 // TODO: Print each one
-
+console.log(text, num, bool, undef);
